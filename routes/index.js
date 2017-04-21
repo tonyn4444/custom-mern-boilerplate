ceb44bd185f4a6e5d5b90b499d92e5fa44bedbeb
Tony Nguyen <tntuyen@ucdavis.edu>
2017-04-21 16:41:53 -0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var middleware = require('../middleware');
+var request = require('request');
 
+var API_KEY = '&APPID=ef328b71e4664319163442c800054e65'
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=sunnyvale,us'
+
+var options = {
+	url: url,
+	headers: {
+		'APPID': API_KEY
+	}
+}
 
 router.get('/', function(req, res) {
-	res.render('index');
+	request(url + API_KEY, function(error, response, body) {
+	if (!error && response.statusCode == 200) {
+		var parsedData = JSON.parse(body);
+		res.render('index', {data: parsedData})
+		}
+	});
 })
 
 module.exports = router;
